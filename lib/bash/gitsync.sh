b999a3f4c734f826da23e8af364135463bcdb656
djtaylor <djtaylor@workvm.com>
2014-01-14 09:03:11 +0700

    Removed verbose output for rsync command when running a Git sync.

--- lib/bash/gitsync.sh
+++ lib/bash/gitsync.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 # Change to a temporary working directory
-mkdir /tmp/m7_git_sync
-cd /tmp/m7_git_sync
+mkdir /tmp/m7; cd /tmp/m7
 
 # Clone the Git repository
 git clone https://github.com/djtaylor/m7.git
 
 # Rsync the directories
-rsync -a -v /tmp/m7_git_sync/m7/ /opt/vpls/m7/.
+rsync -a /tmp/m7/m7/ /opt/vpls/m7/.
 
 # Leave the working directory and delete it
-cd && rm -rf /tmp/m7_git_sync
+cd && rm -rf /tmp/m7
 
 # Delete the local '.git' directory
 rm -rf ~/.git
 
 # Delete the '.gitignore' files
 rm -f ~/output/.gitignore
-rm -f ~/db/.gitignore
 rm -f ~/log/.gitignore
 rm -f ~/plans/.gitignore
 rm -f ~/lock/subsys/.gitignore
